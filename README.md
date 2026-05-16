1f9ac9db2d24c00e842cae102524db71e0fd6f36
ejohnson22 <ejohnson22@georgefox.edu>
2026-05-16 11:28:38 -0700

    Welcome to paradise 🌴

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aisle Island 🌴
+Welcome to Aisle Island, a wonderful place to be.
+
+This repository stores scripts to automatically configure
+a Linux sysytem to my liking.
---
+++ setup.sh
@@ -0,0 +1,23 @@
+## Create docs directory to store system documentation
+[ ! -d "$HOME/docs" ] && mkdir "$HOME/docs"
+
+## Create repos directory to store Git repositories
+[ ! -d "$HOME/repos" ] && mkdir "$HOME/repos"
+
+## Create sandbox directory so that we can blow things up in an isolated place
+[ ! -d "$HOME/sandbox" ] && mkdir "$HOME/sandbox"
+
+## Create scripts directory to store custom scripts
+[ ! -d "$HOME/scripts" ] && mkdir "$HOME/scripts"
+
+## Copy bash_aisle to user home
+cp .bash_aisle "$HOME/.bash_aisle"
+
+## Append bash_aisle to end of bashrc
+echo >> "$HOME/.bashrc"
+echo '# Aisle Island' >> "$HOME/.bashrc"
+echo '. .bash_aisle' >> "$HOME/.bashrc"
+
+## Source changes
+. "$HOME/.bashrc"
+echo 'Successfully set up Aisle Island. Welcome to paradise 🌴'
